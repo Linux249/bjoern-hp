a0d52bc007def22904950f0b0631df50ba1115e1
julian <julian.libor@gmail.com>
2017-09-08 17:15:39 +0200

    style Header

--- src/style/App.js
+++ src/style/App.js
@@ -3,5 +3,11 @@ import styled from 'styled-components'
 
 export default styled.div`
     background-color: #fff;
+    
+    display: flex;
+    align-items: center;
+    flex-flow: column;
+    
+    
         
 `
--- src/style/Header.js
+++ src/style/Header.js
@@ -2,8 +2,10 @@
 import styled from 'styled-components'
 
 export default styled.div`
+    width: 90%;
+    max-width: 1000px;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     height: 200px;
     
 `

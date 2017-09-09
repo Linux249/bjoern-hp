2bed7f1effbe2349dab78a7d4aac902327ebc656
Julian Libor <julian.libor@gmail.com>
2017-09-09 15:03:49 +0200

    add imgs

--- src/pages/Bilder.js
+++ src/pages/Bilder.js
@@ -1,3 +1,28 @@
 import React from 'react'
+import Image from '../components/Image'
 
-export default () => <div>comming soon</div>
+import img1 from '../img/1.jpeg'
+import img2 from '../img/2.jpeg'
+import img3 from '../img/3.jpeg'
+import img4 from '../img/4.jpeg'
+import img5 from '../img/5.jpeg'
+import img6 from '../img/6.jpeg'
+import img7 from '../img/7.jpeg'
+import img8 from '../img/8.jpeg'
+import img9 from '../img/9.jpeg'
+import img10 from '../img/10.jpeg'
+import img11 from '../img/11.jpeg'
+
+export default () => <div>
+    <Image img={img1}/>
+    <Image img={img2}/>
+    <Image img={img3}/>
+    <Image img={img4}/>
+    <Image img={img5}/>
+    <Image img={img6}/>
+    <Image img={img7}/>
+    <Image img={img8}/>
+    <Image img={img9}/>
+    <Image img={img10}/>
+    <Image img={img11}/>
+</div>

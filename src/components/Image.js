d824b62faa0486ddc5940fbe75a43d943ff2dd34
Julian Libor <julian.libor@gmail.com>
2017-09-09 14:57:42 +0200

    add img

---
+++ src/components/Image.js
@@ -0,0 +1,11 @@
+/**
+ * Created by libor on 09.09.2017.
+ */
+import React from 'react'
+import Image from '../style/Image'
+import Menu from '../style/Menu'
+import Title from '../style/Titel'
+import { Link } from 'react-router-dom'
+
+export default ({ img }) => <Image src={img} />
+
---
+++ src/style/Image.js
@@ -0,0 +1,8 @@
+/**
+ * Created by libor on 09.09.2017.
+ */
+import styled from 'styled-components'
+
+export default styled.img`
+    
+`

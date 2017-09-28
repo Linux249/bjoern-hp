ce1cdea938a4244a79694d2f928207d27b595048
Julian Libor <julian.libor@gmail.com>
2017-09-28 12:23:32 +0200

    add sozial media to contact

--- src/pages/Kontakt.js
+++ src/pages/Kontakt.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import Page from '../style/Page'
+import Instagram from '../img/Instagram'
+import Facebook from '../img/Facebook'
 
-
-export default () => <div>comming soon</div>
+export default () => <Page>
+    <a href="https://www.instagram.com/bjoernruppert/"><Instagram/></a>
+    <a href="https://www.facebook.com/bjorn.ruppert"><Facebook/></a>
+</Page>

182541aebdf2810e626811b4a651d50cf7024419
julian libor <julian.libor@gmail.com>
2018-12-26 15:29:32 +0100

    editor config

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    env: {
+        //browser: true,
+        node: true,
+    },
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/airbnb'],
+    // required to lint *.vue files
+    plugins: ['vue'],
+    // check if imports actually resolve
+    /*settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'build/webpack.base.conf.js'
+      }
+    }
+  },*/
+    // add your custom rules here
+    rules: {
+        /*// don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never'
+    }],
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e' // for e.returnvalue
+      ]
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      optionalDependencies: ['test/unit/index.js']
+    }],*/
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+        // allow debugger during development
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+        // custom indentation
+        'indent': ['error', 4],
+
+        // custom line length
+        'code': 100,
+
+        // allow underscore in class setter/getter/constructor
+        'no-underscore-dangle': ['error', { allowAfterThis: true }],
+
+        // damn arra warning in draw
+        'prefer-destructuring': ['error', { object: true, array: false }],
+        'import/extensions': ['error', 'never', { ignorePackages: true }],
+
+        'no-param-reassign': ["error", { "props": false }],
+    },
+};
--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 Öffentliche Sammlungen: 
 2016 Stadt Heidelberg
 
-Nebentätigkeit:
-Rahmenbauer
 
+Landschaften
+Beton/Räume | Betonwelten
+Linoldrukce
 
  
 "ohne Titel"
@@ -16,13 +17,22 @@ Rahmenbauer
 
 
 TODO
-- [ ] 3 Kategorien auf die startseite
-- [ ] Meta Information zu den Bildern und Bilder größer
+- [x] Meta Information zu den Bildern und Bilder größer
 - [x] Ausstellungen auflisten 
-- [ ] Ausstellungen designen 
 - [x] Überschrift ändern
 - [x] Über mich überarbeiten (neuer Text)
 - [x] Über mich überarbeiten (design)
-- [ ] Kontakt (Adresse/Mail)
+- [x] Kontakt (Adresse/Mail)
+- [ ] Ausstellungen designen 
+- [ ] 3 Kategorien bei den bilder über scrolllinks
+- [ ] 3 Kategorien bei den bilder im header mehr in Focus
+
 
 ein bisschen Design
+
+neuer Text:
+Björn Ruppert, geboren 1980 in Ulm, lebt und arbeitet als freischaffender Künstler in Heidelberg. In seinen Gemälden verfängt sich der Blick in Landschaften aus dezenten Farben. Er ertastet die Grenzen von Natur und Architektur und eröffnet so neue Räume, die die Sicht bishin zum Horizont ermöglichen.
+
+Diese urbanen Ansichten entstehen in einem anhaltenden Übermalungsakt. Die einzelnen Schichten aus Ölfarbe und die zeitliche Hingabe, mit der sich der Maler dem Motiv widmet, kennzeichnen die Intensität der Bilder.
+
+Das Rohe und Ursprüngliche der Ölbilder findet sich auch in den Linoldrucken wieder, die der Künstler, als eine minimalistische Ausdrucksweise für sich entdeckt hat. In beiden Fällen sind die Motive losgelöst von Ort und Zeit und entstehen aus der Vorstellungskraft des Künstlers.
